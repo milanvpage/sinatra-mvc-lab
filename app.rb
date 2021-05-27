c07d5eb65810085e1c0e57aa8da281ae61f095f1
Milan <milanvpage@gmail.com>
2021-05-26 17:29:21 -0700

    commit tree c79290141b42bcd729427a2833027a56af7f494e

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        @user_result = PigLatinizer.new.piglatinize(params[:user_phrase])
+        erb :results
+    end
+
+end 
